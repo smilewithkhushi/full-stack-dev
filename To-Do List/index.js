b2e9df7d59773e0f190ff10be2d01724c00f51d6
Khushi <102166679+smilewithkhushi@users.noreply.github.com>
2022-10-11 00:36:45 +0530

    Add files via upload

---
+++ To-Do List/index.js
@@ -0,0 +1,7 @@
+//function to generate the tasks
+
+function ShowTasks(){
+    var heading=document.getElementsByClassName('task-input');
+    var div=document.getElementById('flex-task-show');
+    div.appendChild()
+}
